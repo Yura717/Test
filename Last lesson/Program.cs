fcfe1c7eff8151addb56a5d53dbb294344fd8b69
Yura717 <yura.sydilo@gmail.com>
2020-12-06 16:53:09 +0200

    First commit

---
+++ Last lesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestWorkWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This work");
+            Console.ReadKey();
+        }
+    }
+}
